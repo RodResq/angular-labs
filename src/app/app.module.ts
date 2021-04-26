78dafd3810d2df03ef99a51f72e42509975e47dc
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-04-25 22:56:31 -0300

    updt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MeuPrimeiroComponent } from '../app/meu-primeiro/meu-primeiro.component'
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component,
   ],
   imports: [
     BrowserModule
