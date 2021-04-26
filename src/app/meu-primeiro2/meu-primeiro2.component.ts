eb1026f21fce8f387ae419831d05b185e86335d4
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-04-25 23:27:02 -0300

    updt

--- src/app/meu-primeiro2/meu-primeiro2.component.ts
+++ src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-meu-primeiro2',
+  selector: 'meu-primeiro2',
   templateUrl: './meu-primeiro2.component.html',
   styleUrls: ['./meu-primeiro2.component.css']
 })
