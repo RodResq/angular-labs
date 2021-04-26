306c284b84508c961a3f798f6a49a0b1e0b75b78
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-04-26 00:26:03 -0300

    logica com objetos literais

--- src/app/cursos/curso-detalhe/curso-detalhe.component.ts
+++ src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -12,4 +12,15 @@ export class CursoDetalheComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getTranslationMaps(rhyme): any {
+    const rhymes = {
+      "apples and pears": "Stairs",
+      "hampstead heath": "Teeth",
+      "loaf of bread": "Head",
+      "pork pies": "Lies",
+      "whistle and flute": "Suit",
+    };
+    return rhymes[rhyme.toLowerCase()] ?? 'Rhyme not found';
+  }
+
 }
