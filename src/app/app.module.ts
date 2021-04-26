c786400130b50b075637e25294a044c50a12dbb0
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-04-26 20:30:21 -0300

    data-biding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import {CursosModule} from './cursos/cursos.module';
+import { DataBidingComponent } from './data-biding/data-biding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiro2Component,
+    DataBidingComponent,
   ],
   imports: [
     BrowserModule,
