b2b3a5d3d2ee2a17e44dfdea2dfe6d49e1e83411
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-01 00:20:43 -0300

    revisao ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CicloComponent } from './ciclo/ciclo.component';
 import { DiretivasComponent } from './diretivas/diretivas.component';
 import { DiretivaNgIfComponent } from './diretiva-ng-if/diretiva-ng-if.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch
     DiretivasComponent,
     DiretivaNgIfComponent,
     DiretivaNgswitchComponent,
+    DiretivaNgforComponent,
   ],
   imports: [
     BrowserModule,
