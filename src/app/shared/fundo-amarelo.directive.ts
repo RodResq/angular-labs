4472f29136197598d99ca7b213ce1d069afebe8f
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-02 00:01:12 -0300

    revisao elementRef e renderer, importante!!!

--- src/app/shared/fundo-amarelo.directive.ts
+++ src/app/shared/fundo-amarelo.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[fundoAmarelo]'
+  selector: 'p[fundoAmarelo]'
 })
 export class FundoAmareloDirective {
 
-  constructor() { }
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer2) {
+    // console.log(this._elementRef);
+    // this._elementRef.nativeElement.style.backgroundColor = 'yellow';
+    this._renderer.setStyle(
+      this._elementRef.nativeElement,
+      'background-color',
+      'yellow');
+  }
 
 }
