23b117a0192ff21e62cd7fae8e7bca86eabcd57f
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-02 19:42:24 -0300

    diretivas customizadas

---
+++ src/app/shared/highlight.directive.ts
@@ -0,0 +1,25 @@
+import {Directive, HostBinding, HostListener, Input} from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective {
+
+  @HostListener('mouseenter') onMouseOver() {
+    this.backgroundColor = this.hightLoghtColor;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  @Input() defaultColor: string = 'white';
+
+  @Input() hightLoghtColor: string = 'yellow';
+
+  constructor(
+  ) { }
+
+}
