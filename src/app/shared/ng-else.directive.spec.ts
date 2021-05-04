ec46d3cef61d10d5a1e4e8b8ce1e986e041cdf89
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-03 21:16:09 -0300

    Diretivas estruturais com ngElse, importante!

--- src/app/shared/ng-else.directive.spec.ts
+++ src/app/shared/ng-else.directive.spec.ts
@@ -2,8 +2,9 @@ import { NgElseDirective } from './ng-else.directive';
 
 describe('NgElseDirective', () => {
   it('should create an instance', () => {
-    // @ts-ignore
     const directive = new NgElseDirective();
     expect(directive).toBeTruthy();
   });
 });
+
+
