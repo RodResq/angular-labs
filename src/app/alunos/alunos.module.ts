8fb330d28a0bf58cdc15ca93f6eade7c99a1b134
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-09 20:12:15 -0300

    revisao modulos de funcionalidade usando rotas!

--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core';
-import {AlunosComponent} from './alunos.component';
 import {CommonModule} from '@angular/common';
+
+import {AlunosComponent} from './alunos.component';
+import {AlunoFormComponent} from './aluno-form/aluno-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 
 @NgModule({
   declarations: [
-    AlunosComponent
+    AlunosComponent,
+    AlunoFormComponent,
+    AlunoDetalheComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/alunos/alunos.routing.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+const alunosRouter = [];
+
+@NgModule({
+  imports: [RouterModule.forChild(alunosRouter)]
+})
+export class AlunosRoutingModule {
+
+}
