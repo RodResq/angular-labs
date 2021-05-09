bad4e8cf974f1d251d12e177ad55da22a27f358e
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-09 10:44:12 -0300

    revisao rotas, queryParams, rota imperativa com queryParams! Importantissimo

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,3 +1,25 @@
+import {NgModule} from '@angular/core';
+
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {CursosComponent} from './cursos/cursos.component';
+import {ModuleWithProviders} from '@angular/core';
+import {CursoDetalheComponent} from './curso-detalhe/curso-detalhe.component';
+import {CursoNaoEncontradoComponent} from './curso-nao-encontrado/curso-nao-encontrado.component';
+
+const appRouting : Routes = [
+  { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent},
+  { path: 'cursoNaoEncontrado', component: CursoNaoEncontradoComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '', component: HomeComponent }
+];
+
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRouting);
+
+
+@NgModule()
 export class AppRoutingModule {
 
 }
