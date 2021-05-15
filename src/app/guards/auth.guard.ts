f7c367428b06c8a9501cbcc2a9dd71f61a082ceb
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-15 20:18:16 -0300

    revisao canLoad! importante

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {AuthService} from '../login/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
     private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.verificarAcesso();
+  }
+
+  private verificarAcesso() {
     if(this.authService.usuarioEstaAutenticado()) {
       return true;
     }
 
     this.router.navigate(['/login']);
     return false;
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log('canLoad: Verificando se o usuario pode carregar o modulo');
+    return this.verificarAcesso();
   }
 }
