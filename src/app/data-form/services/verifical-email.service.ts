e8409f8bc43256a69ada89b52f16fbf3f2755584
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-31 20:32:29 -0300

    criacao json e servico

--- src/app/data-form/services/verifical-email.service.ts
+++ src/app/data-form/services/verifical-email.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerificalEmailService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  verficarEmail(email: string) {
+    return this.httpClient.get('./assets/dados/verificar-email.json')
+  }
 }
