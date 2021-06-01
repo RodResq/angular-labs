cc3c5e018e47b76e0dae2d10f5641af6decb609e
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-05-31 21:16:29 -0300

    dealy com map e tap! filter -> revisao

--- src/app/data-form/services/verifical-email.service.ts
+++ src/app/data-form/services/verifical-email.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map, tap} from 'rxjs/operators';
+import {delay, map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +12,13 @@ export class VerificalEmailService {
   verficarEmail(email: string) {
     return this.httpClient.get('./assets/dados/verifica-email.json')
       .pipe(
+        delay(2000),
         map((dados: {emails: any[]}) => dados.emails),
         tap(console.log),
         map((dados: {email: string}[]) => dados.filter(v => v.email === email)),
-        tap(console.log),
+        // tap(console.log),
         map((dados: any) => dados.length > 0),
-        tap(console.log)
+        // tap(console.log)
       );
   }
 }
