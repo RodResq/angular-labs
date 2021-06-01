00c48a1b978b1a6a6a6966fe80c8ccd8485d749e
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-06-01 20:24:36 -0300

    criacao componente input-field

--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-input-field',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-field.component.css']
 })
 export class InputFieldComponent implements OnInit {
+  @Input() classeCss: any;
 
   constructor() { }
 
