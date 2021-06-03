9f3ed73818c4ece8a22eb0a48d891747ccca024b
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-06-02 23:44:30 -0300

    http -request! revisao

--- src/app/request-http-cursos/request-http-cursos.module.ts
+++ src/app/request-http-cursos/request-http-cursos.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RequestHttpCursosRoutingModule } from './request-http-cursos-routing.module';
+import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CursosListaComponent],
   imports: [
     CommonModule,
     RequestHttpCursosRoutingModule
