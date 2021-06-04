501ef19afe136eb96874d241b2d4365daec10498
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-06-04 20:37:05 -0300

    ajuste servise! SUBJECT! IMPORTANT!

--- src/app/request-http/unsubscribe-rxjs/enviar-valor.service.ts
+++ src/app/request-http/unsubscribe-rxjs/enviar-valor.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnviarValorService {
 
+  private emissor$ = new Subject();
+
   constructor() { }
+
+  emitirValor(valor) {
+    this.emissor$.next(valor);
+  }
 }
