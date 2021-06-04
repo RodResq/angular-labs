7b0482c59f3be6c8d90dea54f7f5f7b56596e6ae
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-06-03 22:37:10 -0300

    chamada http! revisao

--- src/app/request-http-cursos/cursos-lista/cursos.service.ts
+++ src/app/request-http-cursos/cursos-lista/cursos.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Curso} from './curso';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
-  private readonly APi: string = 'http://localhost:3000/cursos';
+  private readonly APi = `${environment.API}cursos`;
 
   constructor(private httpClient: HttpClient) { }
 
