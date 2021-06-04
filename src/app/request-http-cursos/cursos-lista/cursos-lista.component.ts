b93fb7ab8d801b9560d8af184fd0faea23e35f00
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-06-03 22:06:17 -0300

    cricao servicos cursos

--- src/app/request-http-cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/request-http-cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.css']
+  styleUrls: ['./cursos-lista.component.css'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
+  cursos$: Observable<any>;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRefresh() {
+
+  }
+
+  onEdit(id) {
+
+  }
+
+  onDelete(curso: any) {
+
+  }
+
+  onConfirmDelete() {
+
+  }
+
+  onDeclineDelete() {
+
+  }
 }
