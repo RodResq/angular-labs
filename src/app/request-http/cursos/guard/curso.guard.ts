7e54487ac2a9b44cfb95d88481a571a11240c588
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-06-07 21:29:12 -0300

    retorno obsjeto curso vazio, com of RESOLVER!

--- src/app/request-http/cursos/guard/curso.guard.ts
+++ src/app/request-http/cursos/guard/curso.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Resolve} from '@angular/router';
-import { Observable } from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Curso} from '../cursos-lista/curso';
 import {CursosService} from '../cursos-lista/cursos.service';
 
@@ -12,11 +12,15 @@ export class CursoGuard implements Resolve<Curso> {
   constructor(private service: CursosService) {
   }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Curso> | Promise<Curso> | Curso {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Curso> {
 
     if(route.params && route.params['id']) {
       return this.service.loadById(route.params['id']);
     }
+    return of({
+      id: null,
+      nome: null
+    });
   }
 
 }
