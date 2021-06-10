c16bf9f437ec540501f091168e945888e85e55f3
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-06-09 23:50:18 -0300

    criando back-end em node! IMPORTANTE

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UploadFileService} from './upload-file.service';
+import {resolveProvidersRequiringFactory} from '@angular/compiler-cli/src/ngtsc/annotations/src/util';
+import {Subject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
   styleUrls: ['./upload-file.component.css']
 })
-export class UploadFileComponent implements OnInit {
+export class UploadFileComponent implements OnInit, OnDestroy {
 
+  filesSet: Set<File> = new Set<File>();
   fileToUpload: File = null;
+  filesUndescribe: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private service: UploadFileService) { }
 
   ngOnInit(): void {
   }
@@ -20,8 +25,20 @@ export class UploadFileComponent implements OnInit {
     const fileNames = [];
     for (let i = 0; i < files.length; i++) {
       fileNames.push(files.item(i).name);
+      this.filesSet.add(files.item(i));
     }
 
     document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+  }
+
+  onUpload() {
+    if(this.filesSet && this.filesSet.size > 0) {
+      this.filesUndescribe = this.service.upload(this.filesSet, 'http://localhost:8000/upload')
+        .subscribe(response => console.log('Upload Concluido'));
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.filesUndescribe.unsubscribe();
   }
 }
