64ce504add1968e10f33e8cd87ddd236895c980b
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-06-15 00:12:44 -0300

    formularios reativos, prograamcao reativa, rxjs operators! RXJS OPERATORS, IMPORTNTISSIMO!

--- src/app/request-http/reactive-search/lib-search/lib-search.component.ts
+++ src/app/request-http/reactive-search/lib-search/lib-search.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {Observable, pipe, Subscription} from 'rxjs';
+import {Observable} from 'rxjs';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {map, tap} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, filter, map, switchMap, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -16,9 +16,28 @@ export class LibSearchComponent implements OnInit {
   results$: Observable<any>;
   total: any;
 
+  readonly FIELDS = 'name,description,version,homepage';
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+     this.results$ = this.queryField.valueChanges.pipe(
+      map(value => value.trim()),
+      filter(value => value.length > 1),
+      debounceTime(200),
+      distinctUntilChanged(),
+      tap(value => console.log(value)),
+      switchMap(value => {
+        return this.http.get(this.SEARCH_API, {
+          params: {
+            search: value,
+            fields: this.FIELDS
+          }
+        });
+      }),
+        tap((res: any) => this.total = res.total),
+        map((res: any) => res.results)
+    )
   }
 
   onSearch() {
